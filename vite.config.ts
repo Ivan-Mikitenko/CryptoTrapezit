2b01194ce32ed557e0c2b7c1376fd99e4d0d75f2
ivan <imikitenko@neoflex.ru>
2024-04-01 15:43:06 +0500

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -16,4 +15,10 @@ export default defineConfig({
       // '@components': path.resolve(__dirname, './src/components'),
     },
   },
+  build: {
+    outDir: 'build',
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    base: 'https://ivan-mikitenko.github.io/CryptoTrapezit/'
+  }
 })
