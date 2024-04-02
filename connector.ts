993e7e4ac4c3fd0b7b8ee16b50ca9775cbc6c2fb
ivan <imikitenko@neoflex.ru>
2024-04-03 00:42:56 +0500

    add telegram wallet

--- connector.ts
+++
@@ -1,6 +0,0 @@
-import TonConnect from '@tonconnect/sdk';
-
-export const connector = new TonConnect();
-
-// Проверка в localStorage есть ли активная ссесия с wallet
-connector.restoreConnection();
--- src/pages/Home/Header/Header.tsx
+++ src/pages/Home/Header/Header.tsx
@@ -1,27 +1,11 @@
 import { HeaderMain } from '@pages/Home/Header/header.styles.ts';
 import { Desc, H1 } from '../../../styles/text.styles.ts';
 import { TonConnectButton, useTonWallet } from '@tonconnect/ui-react';
-import { connector } from '../../../../connector.ts';
 import { useEffect } from 'react';
 
 const tg = window.Telegram.WebApp;
 export const Header = () => {
 	const id = tg?.initDataUnsafe?.user;
-
-	console.log('user', id?.user);
-	useEffect(
-		() =>
-			console.log(
-				'connector',
-				connector.getWallets().then(res => console.log('res', res))
-			),
-		[]
-	);
-
-	console.log('first_name', tg?.initDataUnsafe?.user?.first_name);
-	console.log('last_name', tg?.initDataUnsafe?.user?.last_name);
-	console.log('username', tg?.initDataUnsafe?.user?.username);
-	console.log('language_code', tg?.initDataUnsafe?.user?.language_code);
 
 	return (
 		<HeaderMain>
